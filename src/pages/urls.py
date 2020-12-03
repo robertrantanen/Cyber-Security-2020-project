e99c1ee2ace3b69f95c117e85d3c42d99b9dc986
Robert <robert.j.rantanen@helsinki.fi>
2020-12-03 11:46:15 +0200

    basic templates

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views import homePageView
+from .views import SignUpView
 
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('', homePageView, name='home'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 
 # Create your views here.
 
 def homePageView(request):
-    return HttpResponse('Hello Web!')
+	template = loader.get_template('pages/index.html')
+	return HttpResponse(template.render())
+    
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'pages/signup.html'
